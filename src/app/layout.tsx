79a9645d6b43a8192b75537b4f5cef0b88e6c36e
seunghoon <tmdgns4321@gmail.com>
2023-02-20 21:12:51 +0900

    scroll-smooth 적용
    hydration 에러 지속 발생시 삭제예정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ko">
+    <html lang="ko" className="scroll-smooth">
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
