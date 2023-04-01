60e3665e4b01edf9d839884a21a9ba6b7f8a8d2c
seunghoon <tmdgns4321@gmail.com>
2023-04-01 17:11:44 +0900

    test: notion api routing test

--- src/app/blog/route.ts
+++ src/app/blog/route.ts
@@ -1,12 +1,20 @@
+const { Client } = require("@notionhq/client");
+
 export const fetchFromNotion = async () => {
-  const res = await fetch(
-    "https://portfolio-seunghoonkang.vercel.app/api/notion"
-  );
-  if (!res.ok) {
-    const res = await fetch("http://localhost:3000/api/notion");
-    const data = await res.json();
-    return JSON.parse(data);
-  }
-  const data = await res.json();
-  return JSON.parse(data);
+  const notionSecret = process.env.NOTION_SECRET;
+  const notionDatabaseId = process.env.NOTION_DATABASE_ID;
+  const notion = new Client({ auth: notionSecret });
+
+  const response = await notion.databases.query({
+    database_id: notionDatabaseId,
+  });
+  //@ts-ignore
+  const rows = response.results.map((res) => res.properties) as Row[];
+  // const test = query.results.map((res) => console.log(res));
+  const rowsStructured: rowStructured = rows.map((row) => ({
+    name: row.name.title[0].text.content,
+    first_name: row.first_name.rich_text?.[0].text.content,
+    url: row.link_to_youtube.url,
+  }));
+  return rowsStructured;
 };
