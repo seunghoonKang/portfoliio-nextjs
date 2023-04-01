a7798fbeb5e261227653e896bf6181e58e95a69f
seunghoon <tmdgns4321@gmail.com>
2023-04-01 17:28:33 +0900

    test: notion api token dotenv

--- src/app/blog/route.ts
+++ src/app/blog/route.ts
@@ -1,12 +1,12 @@
-const { Client } = require("@notionhq/client");
-
+import { Client } from "@notionhq/client";
+import { NOTION_SECRET, NOTION_DATABASE_ID } from "@/config";
 export const fetchFromNotion = async () => {
-  const notionSecret = process.env.NOTION_SECRET;
-  const notionDatabaseId = process.env.NOTION_DATABASE_ID;
+  const notionSecret = NOTION_SECRET;
+  const notionDatabaseId = NOTION_DATABASE_ID;
   const notion = new Client({ auth: notionSecret });
 
   const response = await notion.databases.query({
-    database_id: notionDatabaseId,
+    database_id: <string>notionDatabaseId,
   });
   //@ts-ignore
   const rows = response.results.map((res) => res.properties) as Row[];
