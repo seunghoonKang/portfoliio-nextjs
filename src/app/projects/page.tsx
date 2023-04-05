816a42ee7e725211806be688c40a7adae4e609d3
seunghoon <tmdgns4321@gmail.com>
2023-04-05 17:25:16 +0900

    Feat: 노션 api 설정중
    block, page 데이터 받아오는 법 확인중

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import { Suspense } from "react";
+import Loading from "./loading";
+import { fetchFromNotion } from "./route";
+
+export default async function Projects() {
+  const rows = await fetchFromNotion();
+  return (
+    <div>
+      <Suspense fallback={<Loading />}>
+        {/* {rows.map((row) => {
+          return (
+            <>
+              <div key={row.name} className="flex">
+                <h1>{row.name}</h1>
+                <p>{row.first_name}</p>
+                <Link href={row.url}>{row.url}</Link>
+              </div>
+            </>
+          );
+        })} */}
+      </Suspense>
+    </div>
+  );
+}
